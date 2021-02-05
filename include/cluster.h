971724b75774991a65d0ffd5eb87393fc57b6171
Matthew Marshall <matthew.marshall090396@gmail.com>
2021-02-05 19:33:27 +0000

    Add interface for k++ algorithm.

---
+++ include/cluster.h
@@ -0,0 +1,25 @@
+#ifndef N_BODY_SIM_CLUSTER_H
+#define N_BODY_SIM_CLUSTER_H
+
+#pragma once
+
+#include <type_traits>
+
+#include "types.h"
+
+namespace cluster {
+    template <typename T, typename = typename std::enable_if<std::is_floating_point<T>::value>::type>
+    struct Member {
+        T x, y, z;
+    };
+
+    template <typename Precision, typename = typename std::enable_if<std::is_floating_point<Precision>::value>::type>
+    Precision member_distance_2(Member<Precision> lhs, Member<Precision> rhs);
+
+    template <typename Precision>
+    void cluster::kpp(Member<Precision>* members, ui32 member_count, Member<Precision>* centroids, ui32 centroid_count) {
+};
+
+#include "cluster.inl"
+
+#endif // N_BODY_SIM_CLUSTER_H
