ece7e91db02ab78f098ed10492034f17b9201557
Matthew Marshall <matthew.marshall090396@gmail.com>
2021-03-15 13:54:06 +0000

    Create distribution generator for particles.

---
+++ include/particle.h
@@ -0,0 +1,6 @@
+#include <type_traits>
+
+template <typename Precision, typename = typename std::enable_if<std::is_floating_point<Precision>::value>::type>
+struct Particle {
+    Precision x, y, z;
+};
