168a5cb3b420cad16124574e1fefec34aaed3672
Matthew Marshall <matthew.marshall090396@gmail.com>
2023-06-08 18:58:42 +0100

    Enable GLM features we want and include our types and decorators.

--- include/stdafx.h
+++ include/stdafx.h
@@ -4,6 +4,7 @@
 #pragma once
 
 // Basics
+#include <cstdint>
 #include <cstdlib>
 #include <random>
 
@@ -18,8 +19,18 @@
 #include <iostream>
 
 // GL Maths
+#define GLM_ENABLE_EXPERIMENTAL
+
+#define GLM_FORCE_AVX2
 #define GLM_FORCE_SWIZZLE
+#define GLM_FORCE_SIZE_T_LENGTH
+#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
+
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+
+// Our Stuff
+#include "decorators.hpp"
+#include "types.hpp"
 
 #endif  // N_BODY_SIM_STDAFX_H
