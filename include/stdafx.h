fe6a513ea790fdae8ed3e0103810538d4c12c732
Matthew Marshall <matthew.marshall090396@gmail.com>
2023-06-08 18:29:31 +0100

    Add a pre-compiled header.

---
+++ include/stdafx.h
@@ -0,0 +1,25 @@
+#ifndef N_BODY_SIM_STDAFX_H
+#define N_BODY_SIM_STDAFX_H
+
+#pragma once
+
+// Basics
+#include <cstdlib>
+#include <random>
+
+// Generics
+#include <type_traits>
+
+// Strings
+#include <cstring>
+#include <string>
+
+// Streams
+#include <iostream>
+
+// GL Maths
+#define GLM_FORCE_SWIZZLE
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#endif  // N_BODY_SIM_STDAFX_H
