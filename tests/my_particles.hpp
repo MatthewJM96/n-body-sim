43be215762bba7dd107186c021904622f4f876a2
Matthew Marshall <matthew.marshall090396@gmail.com>
2023-06-18 22:48:05 +0100

    Add force and veloctiy to my particle.

--- tests/my_particles.hpp
+++ tests/my_particles.hpp
@@ -6,11 +6,14 @@
 struct MyParticle2D {
     nbs::f32v2 position;
     size_t     cluster_metadata_idx;
+    nbs::f32v2 force;
+    nbs::f32v2 velocity;
 };
 
 struct MyParticle {
     nbs::f32v3 position;
     size_t     cluster_metadata_idx;
+    nbs::f32v2 force;
 };
 
 #endif  // N_BODY_SIM_TESTS_MY_PARTICLES_HPP
